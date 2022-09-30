4a2d51da3fc59467d2335b5d4d88d413f165289f
kdjordan <k.dean.jordan@gmail.com>
2022-09-29 21:01:36 -0700

    added static data files

---
+++ src/static/account.js
@@ -0,0 +1,16 @@
+const account = {
+    account_number: 2356541,
+    account_name : 'XYZ Telecom',
+    ports: 30,
+    cps: 300,
+    tech_prefix: '1234#',
+    ips: ['207.843.22.12', '207.843.22.15'],
+    current_rate_deck: '/link_to_download_deck',
+    commercial_email: 'commercial@xyztele.com',
+    accounting_email: 'accounting@xyztele.com',
+    rates_email: 'rates@xyztele.com',
+    noc_email: 'noc@xyztele.com',
+    current_balance: 2345.67
+}
+
+export default account
---
+++ src/static/stats.js
@@ -0,0 +1,12 @@
+const stats = {
+    attempts: 8989,
+    attepmts_prev: 7045,
+    minutes: 726,
+    minutes_prev: 623,
+    aloc: .2,
+    aloc_prev: .2,
+    asr: 45,
+    asr_prev: 42
+}
+
+export default stats
